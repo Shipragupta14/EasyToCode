be404b766f0ee46378e4ee2fc8b904b47f58515e
Shipragupta14 <shipragupta1411@gmail.com>
2018-04-13 02:41:32 +0530

    Uber Interview

--- Code/Logical/ShiftZeroes.java
+++ Code/Logical/ShiftZeroes.java
@@ -1,4 +1,7 @@
 //http://blog.gainlo.co/index.php/2016/11/18/uber-interview-question-move-zeroes/
+
+METHOD-1:
+
 import java.io.*;
 import java.util.*;
 class GFG {
@@ -29,4 +32,42 @@ input-
 output-
 2 3 4 0 0 
 TC- O(n)
+*/
+
+
+METHOD-2:
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int [] a=new int[n];
+	//	int l=0;
+		int r=n-1;
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+	    for(int i=0;i<n;i++){
+	        if(i<r){
+	            if(a[i]==0 && a[r]==0){
+	                r--;
+	            }else if(a[i]==0 && a[r]!=0){
+	                a[i]=a[r];
+	                a[r]=0;
+	                r--;
+	            }
+	        }
+	    }
+		System.out.println(Arrays.toString(a));
+	}
+}
+/*
+input-
+5 
+2 0 3 0 4
+output-
+2 3 4 0 0 
+TC- O(n) but with more reduced operations
 */
